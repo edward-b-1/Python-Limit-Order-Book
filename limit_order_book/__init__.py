573a912914c6474be2ee37f6e3cbd06139956f39
edward-b-1 <example@example.com>
2024-04-19 23:16:59 +0100

    fixed import

--- limit_order_book/__init__.py
+++ limit_order_book/__init__.py
@@ -4,4 +4,4 @@ from limit_order_book.limit_order_book import Trade
 from limit_order_book.limit_order_book import LimitOrderBook
 from limit_order_book.limit_order_book import DoubleLimitOrderBook
 
-from limit_order_book.util import parse_price_string_and_convert_to_int_price
+from limit_order_book.util_parse import parse_price_string_and_convert_to_int_price
