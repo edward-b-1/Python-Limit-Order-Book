448134ad534b4360b97192913e68bdbbac3bf47e
edward-b-1 <example@example.com>
2024-04-20 11:02:53 +0100

    cleanup

--- test/test_limit_order_book.py
+++ test/test_limit_order_book.py
@@ -2,8 +2,6 @@
 from limit_order_book.limit_order_book import LimitOrderBook
 from limit_order_book.partial_order import PartialOrder
 
-from limit_order_book.util_parse import parse_price_string_and_convert_to_int_price
-
 
 def test_limit_order_book():
     ticker_pyth = 'PYTH'
