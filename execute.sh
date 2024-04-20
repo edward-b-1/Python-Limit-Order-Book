a85dfa82db975da3f786d6729ce12eb3eaf21b74
edward-b-1 <example@example.com>
2024-04-20 13:18:27 +0100

    enabled more test cases

--- execute.sh
+++ execute.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
-cat input.txt | python3 main.py &&
-cat input0.txt | python3 main.py &&
-cat input1.txt | python3 main.py &&
-cat input2.txt | python3 main.py &&
-cat input3.txt | python3 main.py &&
-cat input4.txt | python3 main.py &&
+echo 'input.txt'
+cat input.txt | python3 main.py
+
+echo ''
+echo 'input0.txt'
+cat input0.txt | python3 main.py
+
+echo ''
+echo 'input1.txt'
+cat input1.txt | python3 main.py
+
+echo ''
+echo 'input2.txt'
+cat input2.txt | python3 main.py
+
+echo ''
+echo 'input3.txt'
+cat input3.txt | python3 main.py
+
+echo ''
+echo 'input4.txt'
+cat input4.txt | python3 main.py
+
+echo ''
+echo 'input4_2.txt'
+cat input4_2.txt | python3 main.py
+
+echo ''
+echo 'input5.txt'
 cat input5.txt | python3 main.py
-#cat input6.txt | python3 main.py
+
+echo ''
+echo 'input6.txt'
+# #cat input6.txt | python3 main.py
