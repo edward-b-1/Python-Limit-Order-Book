76acb48c5e7ba5c958819fb91e8fcaf3328fa803
edward-b-1 <example@example.com>
2024-06-29 17:58:35 +0100

    implemented number of orders and top of book functions

--- new_limit_order_book/limit_order_book_wrapper.py
+++ new_limit_order_book/limit_order_book_wrapper.py
@@ -4,6 +4,8 @@ from new_limit_order_book.types.order_id import OrderId
 from new_limit_order_book.order_side import OrderSide
 from new_limit_order_book.order import Order
 from new_limit_order_book.trade import Trade
+from new_limit_order_book.ticker import Ticker
+from new_limit_order_book.top_of_book import TopOfBook
 from new_limit_order_book.multi_ticker_limit_order_book import MultiTickerLimitOrderBook
 
 
@@ -18,10 +20,16 @@ class LimitOrderBook():
         if order.to_volume().is_not_zero():
             self._multi_ticker_limit_order_book.insert(order)
 
+        number_of_orders = self.number_of_orders()
+        print(f'number of orders: {number_of_orders}')
+
         return trades
 
     def order_modify(self, order: Order) -> list[Trade]:
         modified_order = self._multi_ticker_limit_order_book.update(order)
+
+        number_of_orders = self.number_of_orders()
+        print(f'number of orders: {number_of_orders}')
 
         if modified_order is not None:
             trades = self._multi_ticker_limit_order_book.trade(modified_order)
@@ -36,10 +44,23 @@ class LimitOrderBook():
     def order_cancel(self, order_id: OrderId) -> Order|None:
         order = self._multi_ticker_limit_order_book.cancel(order_id)
 
-        if order is None:
-            raise RuntimeError(f'LimitOrderBook.cancel failed to cancel order with order id {order_id}')
+        number_of_orders = self.number_of_orders()
+        print(f'number of orders: {number_of_orders}')
+
+        #if order is None:
+        #    raise RuntimeError(f'LimitOrderBook.cancel failed to cancel order with order id {order_id}')
 
         return order
 
+    def top_of_book(self, ticker: Ticker) -> TopOfBook:
+        number_of_orders = self.number_of_orders()
+        print(f'number of orders: {number_of_orders}')
 
+        top_of_book = self._multi_ticker_limit_order_book.top_of_book(ticker)
+        return top_of_book
+
+    def number_of_orders(self) -> int:
+        number_of_orders = self._multi_ticker_limit_order_book.number_of_orders()
+        print(f'number of orders: {number_of_orders}')
+        return number_of_orders
 
