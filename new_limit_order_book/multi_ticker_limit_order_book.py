493273ebd01fbe4b3e568a8d97fa5fe1774fa487
edward-b-1 <example@example.com>
2024-06-29 18:00:03 +0100

    fixed bug where ticker was not being initialized by insert function, fixed bug in update function, implemented number of orders and top of book function

--- new_limit_order_book/multi_ticker_limit_order_book.py
+++ new_limit_order_book/multi_ticker_limit_order_book.py
@@ -6,6 +6,7 @@ from new_limit_order_book.types.volume import Volume
 from new_limit_order_book.order_side import OrderSide
 from new_limit_order_book.trade import Trade
 from new_limit_order_book.order import Order
+from new_limit_order_book.top_of_book import TopOfBook
 
 from new_limit_order_book.double_limit_order_book import DoubleLimitOrderBook
 
@@ -25,6 +26,16 @@ class MultiTickerLimitOrderBook:
     def __str__(self) -> str:
         pass
 
+    def number_of_orders(self) -> int:
+        return (
+            sum(
+                map(
+                    lambda limit_order_book: limit_order_book.number_of_orders(),
+                    self._limit_order_books.values(),
+                )
+            )
+        )
+
     def trade(self, taker_order: Order) -> list[Trade]:
         ticker = taker_order.to_ticker()
         self._initialize_ticker(ticker)
@@ -33,32 +44,41 @@ class MultiTickerLimitOrderBook:
         return trades
 
     def insert(self, order: Order) -> None:
+        ticker = order.to_ticker()
+        self._initialize_ticker(ticker)
+
         order_id = order.to_order_id()
 
         if self.order_id_exists(order_id):
             raise RuntimeError(f'cannot insert order with existing order id {order_id}')
 
-        ticker = order.to_ticker()
         self._limit_order_books[ticker].insert(order)
 
-    def update(self, order: Order) -> Order|None:
-        price_level_changed_orders = (
-            list(
-                filter(
-                    lambda order: order is not None,
-                    map(
-                        lambda double_limit_order_book: double_limit_order_book.update(order),
-                        self._limit_order_books.values(),
-                    )
-                )
-            )
-        )
+        print(self._limit_order_books[ticker].top_of_book())
 
-        # the above filter logic means that this is actually not possible
-        assert len(price_level_changed_orders) <= 1, f'MultiTickerLimitOrderBook.update invalid number of modified price level orders'
-        if len(price_level_changed_orders) == 1:
-            return price_level_changed_orders[0]
-        return None
+    def update(self, order: Order) -> Order|None:
+        ticker = order.to_ticker()
+        self._initialize_ticker(ticker)
+
+        price_level_changed_order = self._limit_order_books[ticker].update(order)
+        return price_level_changed_order
+        # price_level_changed_orders = (
+        #     list(
+        #         filter(
+        #             lambda order: order is not None,
+        #             map(
+        #                 lambda double_limit_order_book: double_limit_order_book.update(order),
+        #                 self._limit_order_books.values(),
+        #             )
+        #         )
+        #     )
+        # )
+
+        # # the above filter logic means that this is actually not possible
+        # assert len(price_level_changed_orders) <= 1, f'MultiTickerLimitOrderBook.update invalid number of modified price level orders'
+        # if len(price_level_changed_orders) == 1:
+        #     return price_level_changed_orders[0]
+        # return None
 
     def cancel(self, order_id: OrderId) -> Order|None:
         cancelled_orders = (
@@ -97,4 +117,19 @@ class MultiTickerLimitOrderBook:
 
     def _initialize_ticker(self, ticker: Ticker) -> None:
         if not ticker in self._limit_order_books:
+            # print(f'')
+            # print(f'!!! WARNING INITIALIZING TICKER {ticker} !!!')
+            # print(f'')
+            # for key in self._limit_order_books.keys():
+            #     print(f'{key}')
+            # print(f'')
+            print(f'initialize ticker {ticker}')
             self._limit_order_books[ticker] = DoubleLimitOrderBook(ticker)
+
+
+    def top_of_book(self, ticker: Ticker) -> TopOfBook:
+        self._initialize_ticker(ticker)
+
+        top_of_book = self._limit_order_books[ticker].top_of_book()
+        return top_of_book
+
