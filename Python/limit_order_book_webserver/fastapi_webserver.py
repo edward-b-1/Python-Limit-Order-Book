4c2d5b3ed22fa9a7348d73fa5e06f18a546d23a6
remote developer <example@example.com>
2024-07-09 14:18:00 +0000

    fixed origins CORS

--- Python/limit_order_book_webserver/fastapi_webserver.py
+++ Python/limit_order_book_webserver/fastapi_webserver.py
@@ -44,11 +44,8 @@ limit_order_book = LimitOrderBookLogged()
 
 
 origins = [
-    'http://localhost:5555',
-    'http://localhost:80',
-    'http://python-limit-order-book.co.uk:5555',
-    'http://python-limit-order-book.co.uk:5173',
-    'http://python-limit-order-book.co.uk:80',
+    'http://www.python-limit-order-book.co.uk',
+    'http://python-limit-order-book.co.uk',
 ]
 
 print(f'__name__={__name__}')
@@ -56,6 +53,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    #allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
