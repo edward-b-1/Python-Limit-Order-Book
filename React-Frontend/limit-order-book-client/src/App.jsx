ce02da5c10a46db7e8b1c6216ca2e688cca1e9b7
edward-b-1 <example@example.com>
2024-07-10 11:07:31 +0100

    removed fqdn from api request calls

--- React-Frontend/limit-order-book-client/src/App.jsx
+++ React-Frontend/limit-order-book-client/src/App.jsx
@@ -42,14 +42,14 @@ function App() {
   const fetchRows = async () => {
     let rows = [];
     const responseAllTickers = await axios.post(
-      `${import.meta.env.VITE_REACT_API_URL}/api/list_all_tickers`,
+      `/api/list_all_tickers`,
       {}
     );
     const allTickers = responseAllTickers.data['tickers'];
     for (const ticker of allTickers) {
       console.log(`${ticker}`);
       const responseTopOfBook = await axios.post(
-        `${import.meta.env.VITE_REACT_API_URL}/api/top_of_book`,
+        `/api/top_of_book`,
         {
           ticker: ticker,
         }
@@ -86,7 +86,7 @@ function App() {
       `send order: ${sendOrderTicker} ${sendOrderOrderSide} ${sendOrderPrice} ${sendOrderVolume}`
     );
     axios
-      .post(`${import.meta.env.VITE_REACT_API_URL}/api/send_order`, {
+      .post(`/api/send_order`, {
         ticker: sendOrderTicker,
         order_side: sendOrderOrderSide,
         price: sendOrderPrice,
@@ -106,7 +106,7 @@ function App() {
       `update order: ${updateOrderOrderId} ${updateOrderPrice} ${updateOrderVolume}`
     );
     axios
-      .post(`${import.meta.env.VITE_REACT_API_URL}/api/modify_order`, {
+      .post(`/api/modify_order`, {
         order_id: updateOrderOrderId,
         price: updateOrderPrice,
         volume: updateOrderVolume,
@@ -121,7 +121,7 @@ function App() {
   function cancelOrder() {
     console.log(`cancel order: ${cancelOrderOrderId}`);
     axios
-      .post(`${import.meta.env.VITE_REACT_API_URL}/api/cancel_order`, {
+      .post(`/api/cancel_order`, {
         order_id: cancelOrderOrderId,
       })
       .then((response) => {
@@ -134,7 +134,7 @@ function App() {
       `cancel order partial: ${partialCancelOrderOrderId} ${partialCancelOrderVolume}`
     );
     axios
-      .post(`${import.meta.env.VITE_REACT_API_URL}/api/cancel_order_partial`, {
+      .post(`/api/cancel_order_partial`, {
         order_id: partialCancelOrderOrderId,
         volume: partialCancelOrderVolume,
       })
