f8f92b526d545f579e84db9d06dec82618ee7bee
remote developer <example@example.com>
2024-07-11 09:09:57 +0000

    enabled forwarding of proxy headers

--- limit_order_book_webserver/__main__.py
+++ limit_order_book_webserver/__main__.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     print(f'uvicorn run...')
     #config = uvicorn.Config('limit_order_book_webserver:app', port=5000, log_level='info')
     #config = uvicorn.Config('limit_order_book_webserver:app', host='0.0.0.0', port=80, log_level='info')
-    config = uvicorn.Config('limit_order_book_webserver:app', host='0.0.0.0', port=5555, log_level='info')
+    config = uvicorn.Config('limit_order_book_webserver:app', host='0.0.0.0', port=5555, log_level='info', proxy_headers=True)
     server = uvicorn.Server(config)
     server.run()
 
