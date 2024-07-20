6487bb23051ba697623c83c4012842ee44d38334
edward-b-1 <example@example.com>
2024-07-20 17:54:55 +0100

    added human readable datetime to string function

--- lib_datetime/__init__.py
+++ lib_datetime/__init__.py
@@ -13,5 +13,8 @@ def now_string() -> str:
 def datetime_to_string(now: datetime) -> str:
     return now.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
+def datetime_to_order_board_display_string(now: datetime) -> str:
+    return now.strftime('%Y-%m-%d %H:%M:%S.%f %z')
+
 def string_to_datetime(datetime_str: str) -> datetime:
     return datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S.%f%z')
